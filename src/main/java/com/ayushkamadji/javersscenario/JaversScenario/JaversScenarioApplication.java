febada3da94c6c1c31107c506ffdf5f8aab0faed
Ayushka Partohap <ayushka.partohap@happyfresh.com>
2019-11-04 14:47:55 +0700

    Initial commit

---
+++ src/main/java/com/ayushkamadji/javersscenario/JaversScenario/JaversScenarioApplication.java
@@ -0,0 +1,13 @@
+package com.ayushkamadji.javersscenario.JaversScenario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaversScenarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaversScenarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayushkamadji/javersscenario/JaversScenario/JaversScenarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayushkamadji.javersscenario.JaversScenario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaversScenarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
