01f70515a50063009517135ea3235e1059dbfcfa
Ayushka Partohap <ayushka.partohap@happyfresh.com>
2019-11-19 16:35:48 +0700

    Add javers to todo

--- src/main/java/com/ayushkamadji/javersscenario/JaversScenario/repository/TodoRepository.java
+++ src/main/java/com/ayushkamadji/javersscenario/JaversScenario/repository/TodoRepository.java
@@ -1,7 +1,9 @@
 package com.ayushkamadji.javersscenario.JaversScenario.repository;
 
 import com.ayushkamadji.javersscenario.JaversScenario.entity.Todo;
+import org.javers.spring.annotation.JaversSpringDataAuditable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@JaversSpringDataAuditable
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 }
